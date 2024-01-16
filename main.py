c50d952f3cb2ae88cb227d81e4544a39619219cd
XanderG2 <XanderG2@users.noreply.github.com>
2024-01-16 15:46:46 +0000

    player

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ height = 956
 screen = pygame.display.set_mode((width, height))
 
 white = (255,255,255)
+black = (0,0,0)
 
+ph = 100
+pw = 50
+player = pygame.Rect(round(width/2-pw/2), round(height/2-ph/2), pw, ph)
+
+fps = 60
+clock = pygame.time.Clock()
 running = True
 while running:
     for event in pygame.event.get():
@@ -14,6 +21,8 @@ while running:
             running = False
             sys.exit()
     screen.fill(white)
+    pygame.draw.rect(screen, black, player)
     pygame.display.flip()
+    clock.tick(fps)
 
 pygame.quit()
